29e0eeedc314bf104e0c5db2afe3c02fa860d8a3
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-06-03 00:25:38 +0800

    Update test_npu_kimi_k2_6_w4a8_16p_in64k_out1k_100ms.py

--- test/registered/ascend/performance/kimi_k2_6/test_npu_kimi_k2_6_w4a8_16p_in64k_out1k_100ms.py
+++ test/registered/ascend/performance/kimi_k2_6/test_npu_kimi_k2_6_w4a8_16p_in64k_out1k_100ms.py
@@ -1,16 +1,15 @@
 import unittest
 
-from sglang.test.ascend.e2e.test_npu_multi_node_utils import NIC_NAME
 from sglang.test.ascend.e2e.test_npu_accuracy_utils import (
     TestAscendAccuracyTestCaseBase,
 )
+from sglang.test.ascend.e2e.test_npu_multi_node_utils import NIC_NAME
 from sglang.test.ascend.e2e.test_npu_performance_utils import (
     AISBENCHMARK_DATASET_DEFAULT,
     BENCHMARK_TOOL_DEFAULT,
+    KIMI_K2_6_W4A8_MODEL_PATH,
     TestAscendPerformanceTestCaseBase,
-    KIMI_K2_6_W4A8_MODEL_PATH
 )
-
 from sglang.test.ci.ci_register import register_npu_ci
 
 register_npu_ci(
